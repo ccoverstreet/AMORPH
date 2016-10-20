c74bf8d63270bc6c7aa7e9d74a2e01e5f7d6ad58
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-10-21 08:56:13 +1300

    Specify data file on the command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include "DNest4/code/DNest4.h"
+
+// From this project
+#include "Data.h"
 #include "MyModel.h"
 
 int main(int argc, char** argv)
 {
-    DNest4::start<Crystals::MyModel>(argc, argv);
+    // Process command line options
+    DNest4::CommandLineOptions clo(argc, argv);
+
+    // Load data
+    Crystals::MyModel::load_data(clo.get_data_file().c_str());
+
+    // Run DNest4.
+    DNest4::start<Crystals::MyModel>(clo);
+
     return 0;
 }
 
