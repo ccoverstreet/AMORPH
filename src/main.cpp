fbac816ad97574109f925092287c7840c91aa025
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-10-21 12:38:31 +1300

    Default data file

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,13 @@ int main(int argc, char** argv)
     // Process command line options
     DNest4::CommandLineOptions clo(argc, argv);
 
+    // Get specified data file. If none, use a default.
+    std::string data_file = clo.get_data_file();
+    if(data_file.length() == 0)
+        data_file = std::string("50% glass .02step3secdwell.txt");
+
     // Load data
-    Crystals::MyModel::load_data(clo.get_data_file().c_str());
+    Crystals::MyModel::load_data(data_file.c_str());
 
     // Run DNest4.
     DNest4::start<Crystals::MyModel>(clo);
