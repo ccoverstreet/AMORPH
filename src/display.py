7e486a4fe61a2ec908557b96a26f5c9cd1957001
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-10-21 12:47:04 +1300

    Nice display script

--- src/display.py
+++ src/display.py
@@ -7,9 +7,28 @@ def display(data_file="50% glass .02step3secdwell.txt"):
     Function to load and plot the output of a run.
     """
     data = dn4.my_loadtxt(data_file)
+    posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
+    temp = dn4.load_column_names("posterior_sample.txt")
+    indices = temp["indices"]
 
-    plt.plot(data[:,0], data[:,1], "ko", markersize=3)
-    plt.xlabel("$x$", fontsize=16)
-    plt.ylabel("$y$", fontsize=16)
-    plt.show()
+    for i in range(0, posterior_sample.shape[0]):
+        plt.clf()
+
+        # Plot the data
+        plt.plot(data[:,0], data[:,1], "ko", markersize=3)
+        plt.hold(True)
+
+        # Extract the model curve
+        start = indices["model_curve[0]"]
+        end = start + data.shape[0]
+        model = posterior_sample[i, start:end]
+
+        # Plot the data
+        plt.plot(data[:,0], model, "g", linewidth=2)
+        plt.xlabel("$x$", fontsize=16)
+        plt.ylabel("$y$", fontsize=16)
+        plt.show()
+
+if __name__ == "__main__":
+    display()
 
