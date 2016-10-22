188ccfc88773d0b34e91ef2c411434887d38eeb2
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-10-22 16:09:34 +1300

    Nice plots

--- src/display.py
+++ src/display.py
@@ -10,24 +10,51 @@ def display(data_file="50% glass .02step3secdwell.txt"):
     posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
     temp = dn4.load_column_names("posterior_sample.txt")
     indices = temp["indices"]
+    wide_integral = np.zeros(posterior_sample.shape[0])
+    spikes_integral = np.zeros(posterior_sample.shape[0])
+    max_num_spikes = int(posterior_sample[0, indices["max_num_spikes"]])
 
     for i in range(0, posterior_sample.shape[0]):
-        plt.clf()
 
-        # Plot the data
-        plt.plot(data[:,0], data[:,1], "ko", markersize=3)
-        plt.hold(True)
+        if i==0:
+            plt.clf()
+
+            # Plot the data
+            plt.plot(data[:,0], data[:,1], "ko", markersize=3)
+            plt.hold(True)
 
         # Extract the model curve
         start = indices["model_curve[0]"]
         end = start + data.shape[0]
         model = posterior_sample[i, start:end]
 
-        # Plot the data
-        plt.plot(data[:,0], model, "g", linewidth=2)
-        plt.xlabel("$x$", fontsize=16)
-        plt.ylabel("$y$", fontsize=16)
-        plt.show()
+        wide_integral[i] = posterior_sample[i, indices["amplitude"]]*\
+                           posterior_sample[i, indices["width"]]*\
+                           np.sqrt(2*np.pi)
+
+        spikes_a = np.exp(posterior_sample\
+                    [i, indices["log_amplitude[0]"]:\
+                    indices["log_amplitude[0]"] + max_num_spikes])
+        spikes_w = posterior_sample\
+                    [i, indices["width[0]"]:\
+                    indices["width[0]"] + max_num_spikes]
+        spikes_integral[i] = np.sum(spikes_a*spikes_w)*np.sqrt(2*np.pi)
+
+#        print(wide_integral[i], spikes_integral[i])
+
+        # Plot the model
+        if i < 50:
+            plt.plot(data[:,0], model, "g", linewidth=2, alpha=0.1)
+
+    plt.xlabel("$x$", fontsize=16)
+    plt.ylabel("$y$", fontsize=16)
+    plt.show()
+
+    spikes_fraction = spikes_integral/(spikes_integral + wide_integral)
+    plt.hist(spikes_fraction, 100, color=[0.8, 0.8, 0.8])
+    plt.xlim([0, 1])
+    plt.xlabel("(total spike flux)/(total spike flux + wide component flux)")
+    plt.show()
 
 if __name__ == "__main__":
     display()
