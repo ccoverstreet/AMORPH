56134ff75d841d62c644534eee5e3eb6d510b62a
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-07 22:03:55 +1300

    Ask the user for a filename

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,13 @@ int main(int argc, char** argv)
     // Process command line options
     DNest4::CommandLineOptions clo(argc, argv);
 
-    // Get specified data file. If none, use a default.
+    // Get specified data file. If none, ask the user.
     std::string data_file = clo.get_data_file();
     if(data_file.length() == 0)
-        data_file = std::string("easy_data.txt");
+    {
+        std::cout << "# Enter the name of the data file you want to load: ";
+        std::cin >> data_file;
+    }
 
     // Save the data filename
     std::fstream fout("run_data.txt", std::ios::out);
