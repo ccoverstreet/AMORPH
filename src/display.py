d85687184f79a047417a741d7ebf80a5e209c732
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-05-05 10:17:54 +1200

    Try proper DNest4 first

--- src/display.py
+++ src/display.py
@@ -1,4 +1,10 @@
-import dnest_functions as dn4
+# Uses actual DNest4 stuff if it's installed - otherwise
+# uses dnest_functions.py as a fallback
+try:
+    import dnest4.classic as dn4
+except:
+    import dnest_functions as dn4
+
 import numpy as np
 import matplotlib.pyplot as plt
 
