Fixed normalisation

doc/figures.py
@@ -46,8 +46,9 @@ end   = indices["wide[1000]"] + 1
 for i in range(0, 8):
     k = rng.randint(sample.shape[0])
     y = sample[k, start:end]
-    y /= y.max() + 0.00001
+    y /= y.max()# + 0.00001
     y *= np.exp(0.1*rng.randn())
+    
     plt.plot(x, y, "-", alpha=0.4)
     plt.xlabel("$x$")
     plt.ylabel("$y$")
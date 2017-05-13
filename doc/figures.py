71fa74a78c44324be31874e499935b62e6282adf
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-05-13 19:10:04 +1200

    Fixed normalisation

--- doc/figures.py
+++ doc/figures.py
@@ -46,8 +46,9 @@ end   = indices["wide[1000]"] + 1
 for i in range(0, 8):
     k = rng.randint(sample.shape[0])
     y = sample[k, start:end]
-    y /= y.max() + 0.00001
+    y /= y.max()# + 0.00001
     y *= np.exp(0.1*rng.randn())
+    
     plt.plot(x, y, "-", alpha=0.4)
     plt.xlabel("$x$")
     plt.ylabel("$y$")
