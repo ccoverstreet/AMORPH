ea855250cd385d33ef33c837c30d56e694c16516
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-06-08 10:32:11 +1200

    Loading YAML config file

--- src/Config.cpp
+++ src/Config.cpp
@@ -4,8 +4,23 @@
 namespace AMORPH
 {
 
+// Static instance
+Config Config::global;
 
+void Config::load(const char* filename)
+{
 
+    // Load stuff from the YAML file
+    YAML::Node config = YAML::LoadFile(filename);
+
+    data_file = config["data_file"].as<std::string>();
+    dnest4_options_file = config["dnest4_options_file"].as<std::string>();
+    control_points = std::tuple<double, double>();
+    std::get<0>(control_points) = config["control_points"][0].as<double>();
+    std::get<1>(control_points) = config["control_points"][1].as<double>();
+    max_num_narrow_peaks = config["max_num_narrow_peaks"].as<size_t>();
+    max_num_wide_peaks = config["max_num_wide_peaks"].as<size_t>();
+}
 
 
 
--- src/Config.h
+++ src/Config.h
@@ -1,6 +1,9 @@
 #ifndef AMORPH_Config_h
 #define AMORPH_Config_h
 
+#include <string>
+#include <tuple>
+
 namespace AMORPH
 {
 
@@ -8,10 +11,21 @@ class Config
 {
     private:
 
+        std::string data_file;
+        std::string dnest4_options_file;
+
+        std::tuple<double, double> control_points;
+
+        size_t max_num_narrow_peaks;
+        size_t max_num_wide_peaks;
 
     public:
 
+        // Load from YAML file
+        void load(const char* filename);
 
+        // A global instance
+        static Config global;
 
 };
 
